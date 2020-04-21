81f73b47b8449909f9a386db91d42d79238c718f
wm <1046356415@qq.com>
2020-04-21 09:14:34 +0800

    first

---
+++ src/main/java/com/site/blog/my/core/service/impl/ConfigServiceImpl.java
@@ -0,0 +1,92 @@
+package com.site.blog.my.core.service.impl;
+
+import com.site.blog.my.core.dao.BlogConfigMapper;
+import com.site.blog.my.core.entity.BlogConfig;
+import com.site.blog.my.core.service.ConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class ConfigServiceImpl implements ConfigService {
+    @Resource
+    private BlogConfigMapper configMapper;
+
+    public static final String websiteName = "Enjoy Life";
+    public static final String websiteDescription = "duang duang duang";
+    public static final String websiteLogo = "/admin/dist/img/logo2.png";
+    public static final String websiteIcon = "/admin/dist/img/favicon.png";
+
+    public static final String yourAvatar = "/admin/dist/img/pk.jpg";
+    public static final String yourEmail = "1046356415@qq.com";
+    public static final String yourName = "test";
+
+    public static final String footerAbout = "your personal blog. have fun.";
+    public static final String footerICP = "鲁ICP备 xxxxxx-x号";
+    public static final String footerCopyRight = "@2020 Kennys";
+    public static final String footerPoweredBy = "Enjoy Life";
+    public static final String footerPoweredByURL = "##";
+
+    @Override
+    public int updateConfig(String configName, String configValue) {
+        BlogConfig blogConfig = configMapper.selectByPrimaryKey(configName);
+        if (blogConfig != null) {
+            blogConfig.setConfigValue(configValue);
+            blogConfig.setUpdateTime(new Date());
+            return configMapper.updateByPrimaryKeySelective(blogConfig);
+        }
+        return 0;
+    }
+
+    @Override
+    public Map<String, String> getAllConfigs() {
+        //获取所有的map并封装为map
+        List<BlogConfig> blogConfigs = configMapper.selectAll();
+        Map<String, String> configMap = blogConfigs.stream().collect(Collectors.toMap(BlogConfig::getConfigName, BlogConfig::getConfigValue));
+        for (Map.Entry<String, String> config : configMap.entrySet()) {
+            if ("websiteName".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(websiteName);
+            }
+            if ("websiteDescription".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(websiteDescription);
+            }
+            if ("websiteLogo".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(websiteLogo);
+            }
+            if ("websiteIcon".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(websiteIcon);
+            }
+            if ("yourAvatar".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(yourAvatar);
+            }
+            if ("yourEmail".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(yourEmail);
+            }
+            if ("yourName".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(yourName);
+            }
+            if ("footerAbout".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(footerAbout);
+            }
+            if ("footerICP".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(footerICP);
+            }
+            if ("footerCopyRight".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(footerCopyRight);
+            }
+            if ("footerPoweredBy".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(footerPoweredBy);
+            }
+            if ("footerPoweredByURL".equals(config.getKey()) && StringUtils.isEmpty(config.getValue())) {
+                config.setValue(footerPoweredByURL);
+            }
+        }
+        return configMap;
+    }
+}
---
+++ src/test/java/com/site/blog/my/core/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.blog.my.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
